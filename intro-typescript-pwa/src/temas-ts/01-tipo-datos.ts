ce9c9741516de046d1022776c2963d9fc34b3500
Monreal Julia <19002494@alumnos.utleon.edu.mx>
2022-09-12 20:43:48 -0500

    02

--- intro-typescript-pwa/src/temas-ts/01-tipo-datos.ts
+++ intro-typescript-pwa/src/temas-ts/01-tipo-datos.ts
@@ -6,3 +6,15 @@ console.log('Hola Mundo!');
     ===== Código de TypeScript =====
 */
 
+var t1:number | string = 3; // no suele usarse ya que es una variable global
+var t2:number | string = 2; // se puede reasignar un valor y solo se usa donde 
+                            // se declara
+let activo:boolean = false;
+
+const t3 = 666; // es un valor constante.
+
+
+t1 = 'www';
+t2 = 'www';
+
+
--- intro-typescript-pwa/src/temas-ts/02-obj-array-inter.ts
+++ intro-typescript-pwa/src/temas-ts/02-obj-array-inter.ts
@@ -1,8 +1,40 @@
 
+let nombres:string[] = ['Mario', 'Dario', 'Juan']
 
-console.log('Hola Mundo!');
+console.log(nombres);
 
-/*
-    ===== Código de TypeScript =====
-*/
+let datos:(boolean|number|string)[] = ['Mario', 23, true, 'Lopez']
+console.log(datos);
 
+
+datos.push(200);
+datos.push('Mario');
+datos.push(false);
+console.log(datos);
+
+//otra forma de crear arreglos
+let arreglo: Array<number> = [1,2,3,4]
+
+const nombre1:string = 'datos1';
+
+interface Alumno{
+    matricula:string, 
+    nombre:string, 
+    edad:number,
+    email:string,
+    materias?:string[]
+
+    //se agrega el signo de interrogación como opcional
+}
+const alumno:Alumno ={
+
+    matricula:'19002494',
+    nombre:'Julia',
+    edad: 22,
+    email: 'monreal_julia@hotmail.com',
+    materias:['PWA']
+}
+
+alumno.nombre = 'Ohrstrom';
+
+console.log(alumno);
